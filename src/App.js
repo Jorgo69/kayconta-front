82c31c663ebbb2e11c2ded89dcf4c87684b6f9f6
Jorgo69 <Ibralejorgo@gmail.com>
2024-02-02 22:14:29 +0100

    Debut du remake front kayconta

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Template from './components/templates';
+
+function App() {
+  return (
+  <div>
+    <Template />
+  </div>
+  );
+}
+
+export default App;
